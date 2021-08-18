7e41dfa58abe7aa24b3ac1b1db05dc909914d080
Ildar Battalov <battalovia@gmail.com>
2021-08-18 19:58:46 +0400

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString();
+    let arr = str.split('');
+    let newarr = arr.reverse();
+    let newstr = newarr.join('');
+    let newn = parseInt(newstr);
+    return newn;
 }
